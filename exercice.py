f273def6d8ac8ef26a788a0493e133e3eeb22009
Abdul Chaarani <75145721+abdulchaarani@users.noreply.github.com>
2022-09-13 12:05:18 -0400

    exercices fini

--- exercice.py
+++ exercice.py
@@ -7,7 +7,8 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    return string[0:2] + string[3:len(string)]
+    return string.replace(string[2], '')
+    # return string[0:2] + string[3:len(string)]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
